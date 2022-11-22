76f1b0eea48cbd7f01279d1f4b804a8336dae6bc
r1beirin <mateusriibeiro@gmail.com>
2022-11-22 12:02:12 -0300

    add bubblesort

---
+++ Ordenação/bubbleSort.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void bubbleSort(int *array, int n){
+    int aux, troca, fim = n;
+
+    //  Usa o while para botar o maior elemento no final do vetor
+    //  Quando o maior elemento é colocado no final, a variável fim
+    //  diminui 1 para diminuir o tamanho na comparação.
+    do{
+        troca = -1;
+        
+        //  Percorre todo o vetor
+        for(int i = 0; i < fim - 1; i++){
+
+            //  Faz a troca se o vet[i] é maior que vet[i+1]
+            if(array[i] > array[i+1]){
+                aux = array[i];
+                array[i] = array[i+1];
+                array[i+1] = aux;
+                troca = i;
+            }
+        }
+
+        fim--;
+    } while (troca != -1);
+    
+}
+
+int main(){
+    int vet[5] = {4,5,3,2,1};
+    
+    bubbleSort(vet, 5);
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", vet[i]);
+    }
+
+    return 0;
+}
